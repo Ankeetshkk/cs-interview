99423a6579deec52f4fceb41456b594452103fdf
Alvin Cheung <alvinycheung@gmail.com>
2014-11-16 10:19:04 -0600

    Code and styling clean up

--- client/app/coloredShapes/coloredShapes.service.js
+++ client/app/coloredShapes/coloredShapes.service.js
@@ -3,13 +3,13 @@
 angular.module('csInterviewApp')
   .factory('coloredShapes', function() {
     return {
-      pad: function(n, width) {
+      pad: function(n) {
         var colorStringWidth = 6;
         return n.length >= colorStringWidth ? n : new Array(colorStringWidth - n.length + 1).join('9') + n;
       },
+
       colorsAndShapes: function(string) {
-        var colorsAndShapes = [],
-          self = this;
+        var colorsAndShapes = [];
 
         angular.forEach(string.split(''), function(value) {
           var charCode = value.charCodeAt(0),
@@ -18,7 +18,6 @@ angular.module('csInterviewApp')
             colors = ['#f00', '#f90', '#ff0', '#060', '#30c', '#90f'];
 
           colorsAndShapes.push({
-            //color: '#' + self.pad((charCode % maxColorInteger).toString(16)),
             color: colors[charCode % maxColorInteger],
             shape: charCode % numberOfShapes + 1
           });
